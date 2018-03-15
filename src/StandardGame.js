9c850d55ca8055b6f8e89434ac079bddd12aa362
Simon Jentzsch <simon@slock.it>
2018-03-15 19:24:15 +0100

    fixed tilemap

--- src/StandardGame.js
+++ src/StandardGame.js
@@ -3,8 +3,10 @@
 class StandardGame {
 
   preload() {
-    game.load.tilemap('level3', 'levels/cybernoid.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('tiles', 'levels/CybernoidMap3BG_bank.png', 16, 16)
+    game.load.tilemap('level3', 'levels/NephiLowRes.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tiles', 'levels/X11tiles-20-20.png', 20, 20)
+    //    game.load.tilemap('level3', 'levels/cybernoid.json', null, Phaser.Tilemap.TILED_JSON);
+    //    game.load.image('tiles', 'levels/CybernoidMap3BG_bank.png', 16, 16)
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48)
   }
 
@@ -51,7 +53,7 @@ class StandardGame {
     // prepare the map
     this.map = game.add.tilemap(name)
     this.map.setCollisionByExclusion([7, 32, 35, 36, 47])
-    this.map.addTilesetImage('CybernoidMap3BG_bank.png', 'tiles')
+    this.map.addTilesetImage('X11tiles-20-20.png', 'tiles')
 
     // create a layer for collition
     this.layer = this.map.createLayer(0)
@@ -59,7 +61,7 @@ class StandardGame {
 
     // create main-character
     this.player = game.add.sprite(300, 90, 'dude')
-    this.player.scale.set(0.7)
+    //    this.player.scale.set(0.7)
     this.player.animations.add('left', [0, 1, 2, 3], 10, true)
     this.player.animations.add('turn', [4], 20, true)
     this.player.animations.add('right', [5, 6, 7, 8], 10, true)
